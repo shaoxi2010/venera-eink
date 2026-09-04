52c2ebb236db713622f842727d0fa5acc5fb07a9
shaoxi2010 <shaoxi2010@users.noreply.github.com>
2026-09-05 02:17:55 +0800

    fix: import flutter/physics for Simulation type

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -6,6 +6,7 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
